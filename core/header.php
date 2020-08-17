dd4982b9562cf378034d356aa337fda01ed7e02f
cedricdpt <cedricdpt74000@gmail.com>
2020-08-17 21:35:06 +0200

    sauvegarde du 17/08/2020

---
+++ core/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>job-cleanning|le reseau de la propreté</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--jquery-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!--bootstrap-->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	
+
+</head>
+
+<body>
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+      
+      <a class="navbar-brand" href="#">job-cleanning<span>@</span></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse " id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">acceuil <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">candidats</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">professionnels</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">formations</a>
+          </li>
+        </ul>      
+      </div>
+      <!--menu de connection-->
+    <section class="connection">
+      <div class="login">
+        <a href="core/modal.html">connection</a>
+        <a href="#">s'incrire</a>
+      </div>
+    </section>
+    <!--menu de navigation-->
+    </nav>
---
+++ index.php
@@ -0,0 +1,61 @@
+
+<?php include ("core/header.php");?>
+
+
+<div id="moncarousel" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#moncarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#moncarousel" data-slide-to="1"></li>
+    <li data-target="#moncarousel" data-slide-to="2"></li>
+  </ul>
+
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/agent de nettoyage.jpg" alt="Los Angeles">
+      <div class="carousel-caption">
+    <h3>Los Angeles</h3>
+    <p>We had such a great time in LA!</p>
+    <button type="button" class="btn btn-link">inscription</button>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/laveur de vitre.jpg" alt="Chicago">
+      <div class="carousel-caption">
+    <h3>Los Angeles</h3>
+    <p>We had such a great time in LA!</p>
+    <button type="button" class="btn btn-link">inscription</button>
+  </div>
+    </div>
+     <div class="carousel-item">
+      <img src="images/autolaveuse.jpg" alt="Chicago">
+      <div class="carousel-caption">
+    <h3>Los Angeles</h3>
+    <p>We had such a great time in LA!</p>
+    <button type="button" class="btn btn-link">inscription</button>
+  </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#moncarousel" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#moncarousel" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+
+</div>
+
+<section class="description">
+ <h2>job-cleanning</h2>
+ <h3>la premiére plateforme de recrutement pour le secteur de la proprepté</h3>
+ <p>Nous vous apportons l'aide </p>
+</section>
+
+
+
+
+<?php include ("core/footer.php");?>
